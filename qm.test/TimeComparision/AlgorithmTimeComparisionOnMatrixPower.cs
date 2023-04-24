3ed84d5282b8bf692b8ffd5120f3f986041d6d4e
Mateusz Szymanski <--local>
2023-04-24 19:10:26 +0200

    Auto power matrix time comparision

---
+++ qm.test/TimeComparision/AlgorithmTimeComparisionOnMatrixPower.cs
@@ -0,0 +1,84 @@
+﻿using qm.algorithm.MatrixMultiplication;
+using qm.algorithm.QmAlgorithm;
+using qm.generator;
+using qm.test.Helpers;
+using System.Diagnostics;
+
+namespace qm.test.TimeComparision
+{
+    [TestClass]
+    public class AlgorithmTimeComparisionOnMatrixPower
+    {
+        [TestMethod]
+        public void TestTimeComparision()
+        {
+            int step = 50;
+            var generator = new MatrixGenerator(1000);
+            var algorithmComparisionResult = InitializeAlgorithmResultsDictionary();
+            var algorithms = InitializeAlgorithmDictionary();
+            var playerNumberList = Enumerable.Range(1, 200).Select(x => x * step);
+
+            foreach (var playerNumber in playerNumberList)
+            {
+                var matrix = GetGeneratedMatrix(playerNumber, generator);
+
+                foreach (AlgorithmForTest algorithm in Enum.GetValues(typeof(AlgorithmForTest)))
+                {
+                    RunAlgorithm(matrix, algorithmComparisionResult[algorithm], playerNumber, algorithms[algorithm]);
+                }
+
+                WriterHelper.UpdateFileContent(algorithmComparisionResult, "algorithm-time-comparision-auto-power-matrix-results.txt");
+            }
+        }
+
+        private static void RunAlgorithm(byte[][] matrix, List<string> list, int n, Func<byte[][], List<int>> runAlgorithm)
+        {
+            var timer = new Stopwatch();
+
+            timer.Start();
+            runAlgorithm(matrix);
+            timer.Stop();
+
+            list.Add($"{n} {timer.Elapsed.TotalNanoseconds}");
+        }
+
+        private static byte[][] GetGeneratedMatrix(int n, MatrixGenerator generator)
+        {
+            return generator.GenerateAutoPowerMatrix(n);
+        }
+
+        public enum AlgorithmForTest
+        {
+            //Naive,
+            QmAlgorithmWithNaive,
+            QmAlgorithmWithHybrid,
+            QmAlgorithmWithStrassen,
+        }
+
+        private static Dictionary<AlgorithmForTest, List<string>> InitializeAlgorithmResultsDictionary()
+        {
+            return new Dictionary<AlgorithmForTest, List<string>>
+            {
+                //[AlgorithmForTest.Naive] = new List<string> { "Naive Algorithm" },
+                [AlgorithmForTest.QmAlgorithmWithNaive] = new List<string> { "QM Algorithm with Naive" },
+                [AlgorithmForTest.QmAlgorithmWithHybrid] = new List<string> { "QM Algorithm with Hybrid" },
+                [AlgorithmForTest.QmAlgorithmWithStrassen] = new List<string> { "QM Algorithm with Strassen" }
+            };
+        }
+
+        private static Dictionary<AlgorithmForTest, Func<byte[][], List<int>>> InitializeAlgorithmDictionary()
+        {
+            var naiveMultiplication = new NaiveMultiplication<int>();
+            var strassenMultiplication = new StrassenMultiplication<int>(naiveMultiplication);
+            var hybridMultiplication = new HybridMultiplication<int>(naiveMultiplication, strassenMultiplication);
+
+            return new Dictionary<AlgorithmForTest, Func<byte[][], List<int>>>
+            {
+                //[AlgorithmForTest.Naive] = new NaiveAlgorithm().ConductAlgorithm,
+                [AlgorithmForTest.QmAlgorithmWithNaive] = new QmAlgorithm<int>(naiveMultiplication).ConductAlgorithm,
+                [AlgorithmForTest.QmAlgorithmWithHybrid] = new QmAlgorithm<int>(hybridMultiplication).ConductAlgorithm,
+                [AlgorithmForTest.QmAlgorithmWithStrassen] = new QmAlgorithm<int>(strassenMultiplication).ConductAlgorithm,
+            };
+        }
+    }
+}
